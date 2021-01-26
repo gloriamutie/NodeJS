b44ec7fa69043e5d84d80b2b853f92726391edaf
gloria <mwongeliglo15@gmail.com>
2021-01-27 02:16:04 +0300

    new commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+var port = 3000;
+
+app.get("/", (req, res)=>{
+ res.send("Hello World");
+
+});
+app.listen(port,()=>{
+
+    console.log("server listening on port:" + port);
+    
+});
